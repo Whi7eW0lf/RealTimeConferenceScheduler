e47b4fb3394c1be09018aeab8891f8d0cf10a258
banea9 <banea9@gmail.com>
2020-07-02 16:35:33 +0300

    adding styles to forms

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 const userRoutes = require("./routes/user")
 app.set("view engine", "ejs");
 app.set("views", "views");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(userRoutes)
 
