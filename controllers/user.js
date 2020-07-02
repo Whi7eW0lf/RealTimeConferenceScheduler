492a18d621480230f6797da6ed3733ee29b53353
banea9 <banea9@gmail.com>
2020-07-02 15:26:18 +0300

    adding new contrller for signup

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,18 @@
 exports.getLogin = (req, res, next) => {
-    res.render("login", {pageTitle: 'Login page'})
+    res.render("login", {pageTitle: 'Login page', path: "/"})
+}
+exports.getSignIn = (req, res, next) => {
+    res.render("signup", {pageTitle: 'Login page', path: "/"})
+}
+exports.getConferences = (req, res, next) => {
+    res.render("event-form", {pageTitle: 'Login page', path: "/"})
+}
+exports.addSpeaker = (req, res, next) => {
+    res.render("add-speaker", {pageTitle: 'Add speaker', path: "/"})
+}
+exports.addVenue = (req, res, next) => {
+    res.render("add-venue", {pageTitle: 'Add venue', path: "/"})
+}
+exports.addConference = (req, res, next) => {
+    res.render("add-conference", {pageTitle: 'Add conference', path: "/"})
 }
--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,16 @@ const express = require("express");
 const userController = require("../controllers/user");
 
 const router = express.Router();
+router.get("/", userController.getConferences);
 
-router.get("/", userController.getLogin)
+router.get("/login", userController.getLogin);
 
+router.get("/signup", userController.getLogin);
+
+router.get("/add-speaker", userController.addSpeaker);
+
+router.get("/add-venue", userController.addVenue);
+
+router.get("/add-conference", userController.addConference);
 
 module.exports = router;
