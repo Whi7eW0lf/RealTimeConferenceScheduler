2279b16dae9d2e4a0db5dd23f108ca1bb904370e
banea9 <banea9@gmail.com>
2020-07-03 16:29:55 +0300

    adding changes

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,7 @@
 const Venue = require("../models/venues")
-const speaker = require("../models/speaker")
-const hall = require("../models/hall")
-const staticHall = require("..models/staticHalls")
+const Speaker = require("../models/speaker")
+const Hall = require("../models/hall")
+// const staticHall = require("..models/staticHalls")
 
 exports.getLogin = (req, res, next) => {
     res.render("login", {
@@ -23,35 +23,38 @@ exports.getConferences = (req, res, next) => {
 }
 
 
-exports.postAddSpeaker = (req,res,next) => {
+exports.postAddSpeaker = (req, res, next) => {
     const name = req.body.name;
     const description = req.body.description;
     const imageUrl = req.body.profileImg;
 
-    const speaker = new Speaker({
-        name,
-        description,
-        imageUrl
+    const speaker = new Speaker({name, description, imageUrl});
 
-    });
-
-    speaker.save()
-    .then(result =>{
+    speaker.save().then(result => {
         console.log("Added speaker");
         res.redirect("/")
-    })
-    .catch(err=>console.log("error"))
+    }).catch(err => console.log("error"))
 
 }
 
-exports.postAddSpeaker = (req,res,next) => {
-    const name = req.body.name;
-
-    console.log(name);
-
+exports.addConference = (req, res, next) => {
+    Venue.find().then(venues => {
+        res.render("add-speaker", {
+            venues: venues,
+            pageTitle: 'Add speaker',
+            path: "/"
+        })
+    }).catch(err => console.log(err))
 }
-
-
+exports.addHall = (req, res, next) => {
+    Venue.find().then(venues => {
+        res.render("add-hall", {
+            venues: venues,
+            pageTitle: 'Add speaker',
+            path: "/"
+        })
+    }).catch(err => console.log(err))
+}
 exports.postAddConference = (req, res, next) => {
 
     res.render("add-speaker", {
@@ -60,31 +63,28 @@ exports.postAddConference = (req, res, next) => {
     })
 }
 exports.addSpeaker = (req, res, next) => {
-   
+    Venue.find().then(venues => {
+
         res.render("add-speaker", {
             venues: venues,
             pageTitle: 'Add conference',
             path: "/"
         })
-  
+    }).catch(err => console.log(err))
+
 }
 
-exports.addHall = (req, res, next) => {
+exports.postAddNewHall = (req, res, next) => {
     const name = req.body.name;
     const seats = req.body.seats;
-    const halls = new Hall({
-        name,
-        seats
-    });
+    const halls = new Hall({name, seats});
 
-    halls.save()
-    .then(seats => {
+    halls.save().then(seats => {
         res.render("add-hall", {
             seats: seats,
             name: name,
             pageTitle: 'Add hall',
             path: "/"
         })
-    })
-    .catch(err => console.log(err))
+    }).catch(err => console.log(err))
 }
--- models/hall.js
+++ models/hall.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const hallSchema = new Schema({
-    name : {type: String, required = true},
-    seats: {type: Number, required = true}
+    name : {type: String, required: true},
+    seats: {type: Number, required: true}
 });
 
 module.exports = mongoose.model("Hall", hallSchema);
