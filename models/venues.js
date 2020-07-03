04481a4ad7742d77b0f5f57da17513a3dcfee58f
banea9 <banea9@gmail.com>
2020-07-03 11:31:32 +0300

    adding venue model

---
+++ models/venues.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const conferenceSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+
+    
+})
+
+mongoose.model("conference", conferenceSchema);
