1e8d7c87b090b83b60337714f35dc133ed33657d
Slavi <slaveizlatarev@gmail.com>
2020-07-03 13:33:45 +0300

    post speaker

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const mongoose = require('mongoose');
+const dotenv = require("dotenv");
 require('dotenv/config');
 const app = express();
 
