eb8d6b1716ecdfc8a7ab5a07ba36e4e08e74f1ca
banea9 <banea9@gmail.com>
2020-07-05 14:22:43 +0300

    adding user model and fixing the form for hall

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose');
 // require('dotenv/config');
 const app = express();
 
+const User = require("./models/user");
 
 const userRoutes = require("./routes/user");
 
@@ -15,16 +16,31 @@ app.set("views", "views");
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, "public")));
-
+app.use((req, res, next) => {
+    User.findById("5f01b42958989b1dd0878bef")
+    .then(user => {
+        req.user=user;
+        next();
+    })
+    .catch(err => console.log(err));
+})
 app.use(userRoutes)
 
 
 mongoose.connect("mongodb+srv://banea9:stonnerexe95@conference-scheduler.m9skm.mongodb.net/conference?retryWrites=true&w=majority", {
     useUnifiedTopology: true,
     useNewUrlParser: true
-},).then(result => {
+}).then(result => {
     console.log("Connected to DB")
-
-    app.listen(3000)
-
-}).catch(err => console.log(err))
+    User.findOne()
+    .then((user) => {
+        if (!user) {
+            const user = new User({email: "test@test.com", password: "123"})
+            return user.save()
+        }
+    })
+    .then(() => {
+        app.listen(3000)
+    })
+    .catch(err => console.log(err))
+})
