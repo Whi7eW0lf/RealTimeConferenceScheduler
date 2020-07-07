fd89020129afa20a19ad82eabaa16315af2b0ac4
banea9 <banea9@gmail.com>
2020-07-07 08:58:07 +0300

    changing routes user.js file

--- routes/user.js
+++ routes/user.js
@@ -10,13 +10,13 @@ router.post("/add-speaker", isAuthenticated, userController.postAddSpeaker);
 router.get("/add-hall", isAuthenticated, userController.addHall);
 router.post("/add-new-hall", isAuthenticated, userController.postAddNewHall);
 
-router.get("/allconferences", isAuthenticated, userController.getConferences);
+router.get("/allconferences", userController.getConferences);
 
 router.get("/add-conference", isAuthenticated, userController.addConference);
 router.post("/add-conference", isAuthenticated, userController.postAddConference);
 
-router.get("/allconferences/:conferenceId", isAuthenticated, userController.getConference)
+router.get("/allconferences/:conferenceId",  userController.getConference)
 
-router.get("/myconference", isAuthenticated, userController.getMyConferences);
+router.get("/myconference", userController.getMyConferences);
 
 module.exports = router;
