e4c8acd6ca4d56d4a0d4be9c1290d4f094f15a9d
banea9 <banea9@gmail.com>
2020-07-07 11:24:32 +0300

    adding changes to flash message

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,11 +1,18 @@
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
 exports.getLogin = (req, res, next) => {
+    let message = req.flash("error");
+
+    if (message.length > 0) {
+        message = message[0]
+    } else {
+        message = null
+    }
     res.render("login", {
         pageTitle: 'Login page',
         isLoggedIn: req.session.isLoggedIn,
         path: "/login",
-        errorMessage: req.flash("error")
+        errorMessage: message
     })
 }
 exports.getSignup = (req, res, next) => {
@@ -20,41 +27,35 @@ exports.getSignup = (req, res, next) => {
 exports.postLogin = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
-    User.findOne({ email: email })
-      .then(user => {
-        if (!user) {
-        //   req.flash("error", "Invalid email or password...");
-          return res.redirect("/login");
+    User.findOne({email: email}).then(user => {
+        if (!user) { // req.flash("error", "Invalid email or password...");
+            return res.redirect("/login");
         }
-        bcrypt
-          .compare(password, user.password)
-          .then(doMatch => {
+        bcrypt.compare(password, user.password).then(doMatch => {
             if (doMatch) {
-              req.session.isLoggedIn = true;
-              req.session.user = user; 
-              return req.session.save(err => {
-                console.log(err);
-                res.redirect("/");
-              });
+                req.session.isLoggedIn = true;
+                req.session.user = user;
+                return req.session.save(err => {
+                    console.log(err);
+                    res.redirect("/");
+                });
             }
             req.flash("error", "Invalid credentials...");
             req.session.save(err => {
-              res.redirect("/login");
+                res.redirect("/login");
             });
-          })
-          .catch(err => {
+        }).catch(err => {
             console.log(err);
             res.redirect("/login");
-          });
-      })
-      .catch(err => console.log("postLogin error"));
-  };
-  
+        });
+    }).catch(err => console.log("postLogin error"));
+};
+
 exports.logout = (req, res, next) => {
-  req.session.destroy(err => {
-    console.log(err);
-    res.redirect("/");
-  })
+    req.session.destroy(err => {
+        console.log(err);
+        res.redirect("/");
+    })
 };
 
 exports.postSignup = (req, res, next) => {
@@ -62,32 +63,31 @@ exports.postSignup = (req, res, next) => {
     const password = req.body.password;
     const confirmPassword = req.body.confirmPassword;
     const role = req.body.role;
-    User.findOne({ email: email })
-      .then(userDoc => {
+    User.findOne({email: email}).then(userDoc => {
         if (userDoc) {
-        //   req.flash(
-        //     "existingEmail",
-        //     "This email has already been used to create registration."
-        //   );
-          return req.session.save(err => {
-            res.redirect("/signup");
-          });
+            // req.flash(
+            //     "existingEmail",
+            //     "This email has already been used to create registration."
+            // );
+            return req.session.save(err => {
+                res.redirect("/signup");
+            });
         }
-        return bcrypt
-          .hash(password, 12)
-          .then(hashedPassword => {
+        return bcrypt.hash(password, 12).then(hashedPassword => {
             const user = new User({
-              email: email,
-              password: hashedPassword,
-              role: role,
-              conferenceOwner: {conferences: []},
-              conferenceAttendee: {conferences: []}
+                email: email,
+                password: hashedPassword,
+                role: role,
+                conferenceOwner: {
+                    conferences: []
+                },
+                conferenceAttendee: {
+                    conferences: []
+                }
             });
             return user.save();
-          })
-          .then(result => {
+        }).then(result => {
             res.redirect("/login");
-          });
-      })
-      .catch(err => console.log(err));
-  };
+        });
+    }).catch(err => console.log(err));
+};
