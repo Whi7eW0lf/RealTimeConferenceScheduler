725a294435032ea3aeaeea6d097bbfacc6a4f0ce
Vladimir Dimitrov <66771887+vladimirdd96@users.noreply.github.com>
2020-07-10 20:42:27 +0300

    buttons again functionalloty

--- controllers/user.js
+++ controllers/user.js
@@ -20,43 +20,47 @@ exports.getIndex = (req, res, next) => {
 
 }
 exports.getConferences = (req, res, next) => {
-    
+
     Conference.find().populate("address").then(conferences => {
-       
+
         res.render("event-form", {
-            pageTitle: "All conferences", isLoggedIn: req.session.isLoggedIn, path: '/all-conferences', conferences: conferences, currentDate: req.date
+            pageTitle: "All conferences",
+            isLoggedIn: req.session.isLoggedIn,
+            path: '/all-conferences',
+            conferences: conferences,
+            currentDate: req.date
         })
     }).catch(err => console.log(err))
 
 }
 exports.getConferenceDetails = (req, res, next) => {
 
-                const confId = req.params.conferenceId;
-                Conference.findOne({_id: confId}).populate("address").then(conf => {
-                    ConferenceSession.find({conferenceId: conf._id}).populate("hallId").then(sessions => {
-                        Hall.find().then(halls => {
-                            sessions = sessions.map(e => {
-                                const startTime = e.startTime.toString().substring(0, 21);
-                                const endTime = e.endTime.toString().substring(0, 21);
-                                return {
-                                    _id: e._id,
-                                    venueId: e.venueId,
-                                    conferenceId: e.conferenceId,
-                                    hallId: e.hallId,
-                                    startTime: startTime,
-                                    endTime: endTime
-                                }
-                            })
-                            res.render("conference-details", {
-                                halls: halls,
-                                pageTitle: conf.name,
-                                isLoggedIn: req.session.isLoggedIn,
-                                path: "/",
-                                conference: conf,
-                                sessions: sessions || []
-                            })
-                        })
-                    })
+    const confId = req.params.conferenceId;
+    Conference.findOne({ _id: confId }).populate("address").then(conf => {
+        ConferenceSession.find({ conferenceId: conf._id }).populate("hallId").then(sessions => {
+            Hall.find().then(halls => {
+                sessions = sessions.map(e => {
+                    const startTime = e.startTime.toString().substring(0, 21);
+                    const endTime = e.endTime.toString().substring(0, 21);
+                    return {
+                        _id: e._id,
+                        venueId: e.venueId,
+                        conferenceId: e.conferenceId,
+                        hallId: e.hallId,
+                        startTime: startTime,
+                        endTime: endTime
+                    }
+                })
+                res.render("conference-details", {
+                    halls: halls,
+                    pageTitle: conf.name,
+                    isLoggedIn: req.session.isLoggedIn,
+                    path: "/",
+                    conference: conf,
+                    sessions: sessions || []
+                })
+            })
+        })
 
-                }).catch(err => console.log(err))
-            }
+    }).catch(err => console.log(err))
+}
